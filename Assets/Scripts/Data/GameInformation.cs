a19191c6545869edeca0a751857d14f556c34ab3
Jprummel <jordiprummel@live.nl>
2017-02-23 16:16:51 +0100

    Lists added and party object

--- Assets/Scripts/Data/GameInformation.cs
+++ Assets/Scripts/Data/GameInformation.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class GameInformation : MonoBehaviour {
 
+    public static List<PlayerCharacter> JoinedPlayers = new List<PlayerCharacter>();
     public static int HighestWave;
     public static int Wave;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 }
