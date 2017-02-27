c1c53c9a86ffa00acd9571248d19e1e55607915e
Lorenzo Hegberg <18927@ma-web.nl>
2017-02-27 13:38:57 +0100

    Kappa123:+1:

---
+++ Assets/Scripts/Characters/Enemies/EnemyAttack.cs
@@ -0,0 +1,27 @@
+﻿//( ͡° ͜ʖ ͡°)
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnemyAttack : MonoBehaviour {
+
+    Enemy _enemy;
+
+	void Awake () {
+        _enemy = GetComponent<Enemy>();
+	}
+	
+	void Update () {
+        Attack();
+	}
+
+    void Attack()
+    {
+        if(_enemy.DistToPlayer <= _enemy.AttackRange)
+        {
+            //ExecuteEvents.Execute<IDamageable>(_enemy.Target, null, (x, y) => x.TakeDamage(_enemy.Damage));
+            Debug.Log("hit");
+        }
+    }
+}
--- Assets/Scripts/Characters/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Characters/Enemies/EnemyMovement.cs
@@ -1,25 +1,28 @@
-﻿using System.Collections;
+﻿//( ͡° ͜ʖ ͡°)
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-    EnemyTargetting _enemyTargetting;
+    Enemy _enemy;
 
     [SerializeField]private float _moveSpeed;
 
-	// Use this for initialization
-	void Awake () {
-        _enemyTargetting = GetComponent<EnemyTargetting>();
-	}
-	
-	// Update is called once per frame
+    void Awake()
+    {
+        _enemy = GetComponent<Enemy>();
+    }
+
 	void Update () {
         Move();
 	}
 
     void Move()
     {
-        transform.position = Vector2.MoveTowards(transform.position, _enemyTargetting.closestTarget, _moveSpeed * Time.deltaTime);
+        if(_enemy.DistToPlayer > _enemy.AttackRange)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _enemy.closestTarget, _moveSpeed * Time.deltaTime);
+        }
     }
 }
