81eec38924d59c72b894f0bdf3b1573d67c0bf57
Jprummel <jordiprummel@live.nl>
2017-03-15 15:21:09 +0100

    オーディオのインポート
    Ōdio no inpōto

---
+++ Assets/Scripts/Audio/PlaySound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+    
+    [SerializeField]private List<AudioClip> _soundEffects = new List<AudioClip>();
+    private AudioSource _audio;
+
+	void Start () {
+        _audio = GetComponent<AudioSource>();
+	}
+
+    public void PlaySFX(int sfxNumber)
+    {
+        _audio.PlayOneShot(_soundEffects[sfxNumber]);
+    }
+}
