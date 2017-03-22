ae2a3d92d3fe64714eeef0c1148d092874f16b2f
Jprummel <jordiprummel@live.nl>
2017-03-22 09:16:23 +0100

    マージエラー修正
    Mājierā shūsei

--- Assets/Scripts/Characters/Enemies/EnemyTargetting.cs
+++ Assets/Scripts/Characters/Enemies/EnemyTargetting.cs
@@ -31,33 +31,6 @@ public class EnemyTargetting : MonoBehaviour {
         InvokeRepeating("CalculateTarget", 1, 0.33f);
     }
 
-    void CheckForObstacles()
-    {
-        //RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 2f);
-        RaycastHit2D hit = Physics2D.CircleCast(transform.position, 1f, Vector2.zero);
-        if (hit.collider != null)
-        {
-<<<<<<< HEAD
-            if (hit.collider.tag == Tags.OBSTACLE)
-            {
-                _target = null;
-            }
-        }
-        else
-        {
-            _oldDist = 420;
-            CalculateTarget();
-=======
-            //Debug.Log(hit.collider.gameObject.name);
->>>>>>> origin/master
-        }
-    }
-
-    void Update()
-    {
-        //CheckForObstacles();
-    }
-
     void CalculateTarget()
     {
         for (int i = 0; i < _players.Count; i++)
