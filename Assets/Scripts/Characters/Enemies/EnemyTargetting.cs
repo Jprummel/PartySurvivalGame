60672de082106e4b82a610f5695a630ac4531427
Lorenzo Hegberg <18927@ma-web.nl>
2017-03-27 11:59:33 +0200

    敵のターゲット設定
    Teki no tāgetto settei

--- Assets/Scripts/Characters/Enemies/EnemyTargetting.cs
+++ Assets/Scripts/Characters/Enemies/EnemyTargetting.cs
@@ -7,7 +7,7 @@ public class EnemyTargetting : MonoBehaviour {
 
     private List<GameObject> _players = new List<GameObject>();
 
-    private float _oldDist = 420;
+    private float _oldDist = 100;
     private GameObject _target;
     public GameObject Target
     {
@@ -28,22 +28,22 @@ public class EnemyTargetting : MonoBehaviour {
     {
         _players.AddRange(GameObject.FindGameObjectsWithTag(Tags.PLAYER));
 
-        InvokeRepeating("CalculateTarget", 1, 0.33f);
+        InvokeRepeating("CalculateTarget", 1, 0.25f);
     }
 
     void CalculateTarget()
     {
         for (int i = 0; i < _players.Count; i++)
         {
-            //if (_players[i].activeSelf)
-            //{
+            if (_players[i].activeSelf)
+            {
                 float newDist = Vector2.Distance(transform.position, _players[i].transform.position);
                 if (newDist < _oldDist)
                 {
                     _target = _players[i];
                 }
                 _oldDist = newDist;
-            //}
+            }
         }
     }
 }
