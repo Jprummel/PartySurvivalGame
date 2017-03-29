271033fafce9a8d2435860d16702a1c4c4d6f94c
Lorenzo Hegberg <18927@ma-web.nl>
2017-03-29 13:41:25 +0200

    チャージアニメ
    Chājianime

--- Assets/Scripts/Abilities/Knight/Charge.cs
+++ Assets/Scripts/Abilities/Knight/Charge.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Charge : Ability {
 
+    [SerializeField]private float _maxChargeSpeed;
+    [SerializeField]private float _chargeSpeed;
+
     [SerializeField]private AnimationClip _windUp;
     [SerializeField]private AnimationClip _charge;
     [SerializeField]private AnimationClip _intoIdle;
+
+    private bool _charging;
 
     private float _chargeTime;
     private float _windUpTime;
@@ -16,17 +21,20 @@ public class Charge : Ability {
     {
         _chargeTime = _charge.length;
         _windUpTime = _windUp.length;
-        _intoIdleTime = _intoIdle.length;
+        _intoIdleTime = _intoIdle.length / 1.5f;
     }
 
     protected override void Update()
     {
         base.Update();
+        if (_usingAbility)
+        {
+            ChargePlayer();
+        }
     }
 
     public override void UseAbility()
     {
-        Debug.Log("charge");
         if(!_usingAbility && _abilityIsReady)
         {
             StartCoroutine(ChargeRoutine());
@@ -35,8 +43,22 @@ public class Charge : Ability {
 
     void ChargePlayer()
     {
-        Vector2 dir = new Vector2(/*looking direction*/, 0);
-        _player.RGB2D.MovePosition(_player.RGB2D.position + dir * Time.deltaTime);
+        if (_charging)
+        {
+            //increase charge speed overtime
+            _chargeSpeed = Mathf.Lerp(_chargeSpeed, _maxChargeSpeed, Time.deltaTime);
+            //float value of transform.forward
+            float xDir = transform.forward.magnitude;
+            if (transform.rotation.y == 1)
+            {
+                //change direction if player changes direction
+                xDir = -xDir;
+            }
+
+            Vector2 dir = new Vector2(xDir, 0);
+            //charge player
+            _player.RGB2D.MovePosition(_player.RGB2D.position + dir * _chargeSpeed * Time.deltaTime);
+        }
     }
 
     public override void CancelAbility()
@@ -50,11 +72,17 @@ public class Charge : Ability {
         _player.CanMove = false;   
         //start animation spaghettios
         _player.CharacterAnimator.SetBool("WindUp",true);
+        //start charge
+        _charging = true;
         yield return new WaitForSeconds(_windUpTime);
         _player.CharacterAnimator.SetBool("WindUp", false);
+
         _player.CharacterAnimator.SetBool("Charge",true);
         yield return new WaitForSeconds(_chargeTime);
+        _charging = false;
+        //end charge
         _player.CharacterAnimator.SetBool("Charge", false);
+
         _player.CharacterAnimator.SetBool("IntoIdle", true);
         yield return new WaitForSeconds(_intoIdleTime);
         _player.CharacterAnimator.SetBool("IntoIdle", false);
@@ -62,6 +90,7 @@ public class Charge : Ability {
         _cooldown = _maxCooldown;
         _abilityIsReady = false;
         _player.CanMove = true;
+        _chargeSpeed = 0f;
         _usingAbility = false;
     }
 }
