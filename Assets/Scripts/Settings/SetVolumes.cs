9313b6d15a5e2c49656674cc22f33b6b3c0a187c
Jprummel <jordiprummel@live.nl>
2017-05-30 11:55:17 +0200

    Audio options and audio routing

--- Assets/Scripts/Settings/SetVolumes.cs
+++ Assets/Scripts/Settings/SetVolumes.cs
@@ -6,48 +6,41 @@ using System.Collections.Generic;
 
 public class SetVolumes : MonoBehaviour {
 
-    //Fill the list in the editor
     [SerializeField]private AudioMixer _audioMixer;
-    [SerializeField]private List<AudioSource> _musicSources         = new List<AudioSource>();
-    [SerializeField]private List<AudioSource> _soundEffectSources   = new List<AudioSource>();
-    private float _musicVolume;
-    private float _sfxVolume;
-	
+
+    void Awake()
+    {
+
+    }
+
 	void Update () {
         SetVolume();
 	}
 
     void SetVolume()
     {
-        //Changes the volume for all music sources and SFX sources to the values in AudioData
-        foreach (AudioSource source in _musicSources)
-        {
-            if (source != null)
-            {
-                source.volume = SettingsInformation.MusicVolume;
-            }
-        }
-
-        foreach (AudioSource source in _soundEffectSources)
-        {
-            if (source != null)
-            {
-                source.volume = SettingsInformation.SoundFXVolume;
-            }
-        }
+        //Changes the volumes of all the audio groups in the audiomixer
+        _audioMixer.SetFloat("Master", SettingsInformation.MasterVolume);
+        _audioMixer.SetFloat("Music", SettingsInformation.MusicVolume);
+        _audioMixer.SetFloat("SoundFX", SettingsInformation.SoundFXVolume);
     }
 
     //Call these functions from a sliders "On Value Change" function and assign that slider
     //to change the volumes
     public void ChangeMusicVolume(Slider volumeSlider)
     {
-        //_audioMixer.SetFloat("Music", volumeSlider);
+        //_audioMixer.SetFloat("Music", volumeSlider.value);
         SettingsInformation.MusicVolume = volumeSlider.value;
     }
 
     public void ChangeSFXVolume(Slider volumeSlider)
     {
-        //_audioMixer.SetFloat("SoundFX", volumeSlider);
+        //_audioMixer.SetFloat("SoundFX", volumeSlider.value);
         SettingsInformation.SoundFXVolume = volumeSlider.value;
+    }
+
+    public void SliderValue(Slider volumeslider, float value)
+    {
+        volumeslider.value = value;
     }
 }
