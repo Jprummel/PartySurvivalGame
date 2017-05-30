9b13f04564af65b502587605a8a709ee407d8110
Jprummel <jordiprummel@live.nl>
2017-05-30 09:57:05 +0200

    Player Options/Settings and shop rework

---
+++ Assets/Scripts/Settings/SetVolumes.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SetVolumes : MonoBehaviour {
+
+    //Fill the list in the editor
+    [SerializeField]private AudioMixer _audioMixer;
+    [SerializeField]private List<AudioSource> _musicSources         = new List<AudioSource>();
+    [SerializeField]private List<AudioSource> _soundEffectSources   = new List<AudioSource>();
+    private float _musicVolume;
+    private float _sfxVolume;
+	
+	void Update () {
+        SetVolume();
+	}
+
+    void SetVolume()
+    {
+        //Changes the volume for all music sources and SFX sources to the values in AudioData
+        foreach (AudioSource source in _musicSources)
+        {
+            if (source != null)
+            {
+                source.volume = SettingsInformation.MusicVolume;
+            }
+        }
+
+        foreach (AudioSource source in _soundEffectSources)
+        {
+            if (source != null)
+            {
+                source.volume = SettingsInformation.SoundFXVolume;
+            }
+        }
+    }
+
+    //Call these functions from a sliders "On Value Change" function and assign that slider
+    //to change the volumes
+    public void ChangeMusicVolume(Slider volumeSlider)
+    {
+        //_audioMixer.SetFloat("Music", volumeSlider);
+        SettingsInformation.MusicVolume = volumeSlider.value;
+    }
+
+    public void ChangeSFXVolume(Slider volumeSlider)
+    {
+        //_audioMixer.SetFloat("SoundFX", volumeSlider);
+        SettingsInformation.SoundFXVolume = volumeSlider.value;
+    }
+}
---
+++ Assets/Scripts/Settings/SettingsInformation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SettingsInformation : MonoBehaviour {
+
+    //Resolution Settings
+    public static int ResolutionWidth;
+    public static int ResoltuionHeight;
+    public static bool IsFullScreen;
+
+    //Audio Settings
+    public static float MasterVolume;
+    public static float SoundFXVolume;
+    public static float MusicVolume;
+
+    //Cutscene settings
+    public static bool SkipCutscenes;
+}
